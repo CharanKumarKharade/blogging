ec0ba486f8738857f0f725f0ade32b9f394fcfa5
Charan Kumar <charankumarkharade2003@gmail.com>
2025-04-26 21:39:42 +0200

    models done

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+    },
+    {
+        'author': 'Jane Smith',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+    },
+]
 def home(request):
-    return HttpResponse("Hello, world. You're at the blog home page.")
+    context = {'posts': posts}
+    return render(request,'blog/home.html', context)
 def about(request):
-    return HttpResponse("About page")
+    return render(request,'blog/about.html', {'title':'about1 title'})
